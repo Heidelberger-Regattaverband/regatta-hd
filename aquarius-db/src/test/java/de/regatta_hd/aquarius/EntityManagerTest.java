fa4006e10b647d93f0e7b853a78621244b417fc5
Markus Ofterdinger <markus.ofterdinger@sap.com>
2020-11-22 20:11:55 +0100

    re-generated model

--- aquarius-db/src/test/java/de/regatta_hd/aquarius/EntityManagerTest.java
+++ aquarius-db/src/test/java/de/regatta_hd/aquarius/EntityManagerTest.java
@@ -1,19 +1,30 @@
 package de.regatta_hd.aquarius;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityNotFoundException;
 import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
-import de.regatta_hd.aquarius.db.Event;
-import de.regatta_hd.aquarius.db.EventId;
+import de.regatta_hd.aquarius.db.model.Comp;
+import de.regatta_hd.aquarius.db.model.CompEntries;
+import de.regatta_hd.aquarius.db.model.Entry;
+import de.regatta_hd.aquarius.db.model.Event;
+import de.regatta_hd.aquarius.db.model.EventId;
+import de.regatta_hd.aquarius.db.model.Offer;
 
 class EntityManagerTest {
 
@@ -47,6 +58,10 @@ class EntityManagerTest {
 		Event event = entityManager.getReference(Event.class, id);
 		Assertions.assertEquals(id.eventID, event.getEventID());
 		Assertions.assertNotNull(event);
+
+		event.getOffers().forEach(this::trace);
+//		event.getComps().forEach(this::trace);
+//		event.getEntrys().forEach(this::trace);
 	}
 
 	@Test
@@ -57,5 +72,41 @@ class EntityManagerTest {
 			// as event with ID == 2 doesn't exist, calling any getter causes an EntityNotFoundException
 			event.getEventID();
 		});
+	}
+
+	@Test
+	@Disabled
+	void testPokal() {
+		CriteriaBuilder builder = entityManager.getCriteriaBuilder();
+		CriteriaQuery<Offer> cq = builder.createQuery(Offer.class);
+		Root<Offer> from = cq.from(Offer.class);
+		cq.select(from);
+
+		TypedQuery<Offer> query = entityManager.createQuery(cq);
+		List<Offer> resultList = query.getResultList();
+
+		resultList.forEach(this::trace);
+	}
+
+	private void trace(Offer offer) {
+		System.out.println("Offer: ID=" + offer.getOfferID() + ", RaceNr=" + offer.getOfferRaceNumber() + " - "
+				+ offer.getOfferLongLabel());
+		offer.getComps().forEach(this::trace);
+	}
+
+	private void trace(Comp comp) {
+//		Label label = entityManager.getReference(Label.class, new LabelId(comp.getCompLabel()));
+		System.out.println("\tComp: ID=" + comp.getCompID() + ", Label=" + comp.getCompLabel() + ", Round="
+				+ comp.getCompRound() + ", HeatNr=" + comp.getCompHeatNumber() + ", Number=" + comp.getCompNumber());
+		comp.getCompEntries().forEach(this::trace);
+	}
+
+	private void trace(Entry entry) {
+		System.out
+				.println("Entry: ID=" + entry.getEntryID() + ": " + entry.getLabel() + ", " + entry.getEntryComment());
+	}
+
+	private void trace(CompEntries compentries) {
+		System.out.println("\t\tCompEntries: ID=" + compentries.getCeId() + ", Lane=" + compentries.getcELane());
 	}
 }
