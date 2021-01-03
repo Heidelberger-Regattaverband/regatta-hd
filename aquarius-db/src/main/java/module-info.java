cc71d0f474cc4d17d98bed0b921e7d31e79a9320
Markus Ofterdinger <markus@ofterdinger.de>
2021-01-03 11:01:05 +0100

    removed dependency to jpa, transitive via hibernate available

--- aquarius-db/src/main/java/module-info.java
+++ aquarius-db/src/main/java/module-info.java
@@ -1,8 +1,8 @@
 module de.regatta_hd.aquarius.db {
 	requires com.google.guice;
 	requires javax.inject;
-	requires java.base;
 	requires java.persistence;
+	requires java.base;
 
 	opens de.regatta_hd.aquarius.db to com.google.guice;
 
