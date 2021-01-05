38c9e1b0c137a305fdb40269e73e95a88546fe38
Markus Ofterdinger <markus.ofterdinger@sap.com>
2021-01-05 14:55:29 +0100

    connect / disconnect menu items

--- ui/src/main/java/de/regatta_hd/ui/PrimaryController.java
+++ ui/src/main/java/de/regatta_hd/ui/PrimaryController.java
@@ -13,15 +13,24 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
 import javafx.stage.Stage;
 
-public class PrimaryController implements Initializable{
+public class PrimaryController implements Initializable {
 
 	@FXML
 	private MenuBar menuBar;
 
+	@FXML
+	private MenuItem databaseConnect;
+
+	@FXML
+	private MenuItem databaseDisconnect;
+
 	@Inject
 	private AquariusDB aquarius;
+
+	private ResourceBundle resources;
 
 	/**
 	 * Handle action related to "About" menu item.
@@ -33,16 +42,43 @@ public class PrimaryController implements Initializable{
 		System.out.println("You clicked on About!");
 	}
 
+	@FXML
+	private void handleDatabaseConnect() {
+		if (!this.aquarius.isOpen()) {
+			DatabaseConnectionDialog dialog = new DatabaseConnectionDialog((Stage) this.menuBar.getScene().getWindow(),
+					true, this.resources);
+			Optional<ConnectionData> result = dialog.showAndWait();
+			if (result.isPresent()) {
+				this.aquarius.open(result.get());
+			}
+		}
+		updateControls();
+	}
+
+	@FXML
+	private void handleDatabaseDisconnect() {
+		this.aquarius.close();
+		updateControls();
+	}
+
+	@FXML
+	private void handleExit() {
+		Platform.exit();
+	}
+
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
+		this.resources = resources;
+
+		updateControls();
+
 		Platform.runLater(() -> {
-			if (!this.aquarius.isOpen()) {
-				DatabaseConnectionDialog dialog = new DatabaseConnectionDialog((Stage) this.menuBar.getScene().getWindow(), true, resources);
-				Optional<ConnectionData> result = dialog.showAndWait();
-				if (result.isPresent()) {
-					this.aquarius.open(result.get());
-				}
-			}
+			handleDatabaseConnect();
 		});
+	}
+
+	private void updateControls() {
+		this.databaseConnect.setDisable(this.aquarius.isOpen());
+		this.databaseDisconnect.setDisable(!this.aquarius.isOpen());
 	}
 }
