c563507362e75048a4ae16eb3bab32a15c667e39
Markus Ofterdinger <markus@ofterdinger.de>
2021-01-31 11:38:35 +0100

    removed GuiceModule

--- ui/src/main/java/de/regatta_hd/ui/GuiceContext.java
+++ ui/src/main/java/de/regatta_hd/ui/GuiceContext.java
@@ -55,6 +55,7 @@ public class GuiceContext implements FXMLLoaderFactory {
 
 		@Override
 		protected void configure() {
+			bind(FXMLLoaderFactory.class).toInstance(GuiceContext.this);
 		}
 
 		@Provides
