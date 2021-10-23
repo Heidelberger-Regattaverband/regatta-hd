d3c437c0ed8a62677cbddfdf52e4a9d9b160cb7a
Markus Ofterdinger <markus.ofterdinger@sap.com>
2021-10-23 18:21:47 +0200

    added logger

--- ui/src/main/java/de/regatta_hd/ui/pane/TaskUtils.java
+++ ui/src/main/java/de/regatta_hd/ui/pane/TaskUtils.java
@@ -1,13 +1,22 @@
 package de.regatta_hd.ui.pane;
 
-import java.util.Objects;
 import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javafx.concurrent.Task;
 import javafx.concurrent.WorkerStateEvent;
 import javafx.event.EventHandler;
 
 public class TaskUtils {
+	private static final Logger logger = Logger.getLogger(TaskUtils.class.getName());
+
+	// executes database operations concurrent to JavaFX operations.
+	private static ExecutorService databaseExecutor = Executors.newFixedThreadPool(1, new DatabaseThreadFactory());;
 
 	private TaskUtils() {
 	}
@@ -36,13 +45,23 @@ public class TaskUtils {
 		if (onSucceededHandler != null) {
 			task.setOnSucceeded(onSucceededHandler);
 		}
+		task.setOnFailed(t -> logger.log(Level.SEVERE, null, task.getException()));
 		return task;
 	}
 
 	public static <V> Task<V> runTask(Task<V> task) {
-		Thread th = new Thread(Objects.requireNonNull(task, "task must not be null"));
-		th.setDaemon(true);
-		th.start();
+		databaseExecutor.submit(task);
 		return task;
+	}
+
+	static class DatabaseThreadFactory implements ThreadFactory {
+		static final AtomicInteger poolNumber = new AtomicInteger(1);
+
+		@Override
+		public Thread newThread(Runnable runnable) {
+			Thread thread = new Thread(runnable, "Database-Connection-" + poolNumber.getAndIncrement() + "-thread");
+			thread.setDaemon(true);
+			return thread;
+		}
 	}
 }
