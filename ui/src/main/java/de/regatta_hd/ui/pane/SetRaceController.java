a875bcc6c01a7a876a977e0c2fb24df81b223357
Markus Ofterdinger <markus.ofterdinger@sap.com>
2022-02-12 18:15:48 +0100

    organize imports

--- ui/src/main/java/de/regatta_hd/ui/pane/SetRaceController.java
+++ ui/src/main/java/de/regatta_hd/ui/pane/SetRaceController.java
@@ -31,7 +31,6 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableColumn.SortType;
 import javafx.scene.control.TableView;
 import javafx.scene.layout.VBox;
 import javafx.util.Pair;
