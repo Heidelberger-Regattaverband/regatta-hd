aa04a1104032314edd7398f29a786e418515397a
Markus Ofterdinger <markus.ofterdinger@sap.com>
2022-03-12 20:45:26 +0100

    support , and .

--- ui/src/main/java/de/regatta_hd/ui/pane/HeatsController.java
+++ ui/src/main/java/de/regatta_hd/ui/pane/HeatsController.java
@@ -4,10 +4,13 @@ import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.apache.commons.lang3.StringUtils;
 
@@ -25,6 +28,8 @@ public class HeatsController extends AbstractRegattaDAOController {
 	private static final String DELIMITER = ";";
 
 	private static final Logger logger = Logger.getLogger(HeatsController.class.getName());
+
+	private static final Pattern delayPattern = Pattern.compile("\\d*[\\.,]?\\d+");
 
 	@FXML
 	private Button refreshBtn;
@@ -82,49 +87,22 @@ public class HeatsController extends AbstractRegattaDAOController {
 	void handleExportOnAction() {
 		disableButtons(true);
 
-		String csvContent = createCsv();
-
-		File file = FxUtils.showSaveDialog(this.refreshBtn.getScene().getWindow(), getText("heats.csv.description"),
-				"*.csv");
-
-		if (file != null) {
-			saveTextToFile(csvContent, file);
-		}
-
-		disableButtons(false);
-	}
-
-	private String createCsv() {
-		StringBuilder builder = new StringBuilder(4096);
-		builder.append("Index").append(DELIMITER).append("RennNr").append(DELIMITER).append("Abtlg").append(DELIMITER)
-				.append("Delay Bahn 1").append(DELIMITER).append("Delay Bahn 2").append(DELIMITER)
-				.append("Delay Bahn 3").append(DELIMITER).append("Delay Bahn 4").append(DELIMITER).append("Boot Bahn 1")
-				.append(DELIMITER).append("Boot Bahn 2").append(DELIMITER).append("Boot Bahn 3").append(DELIMITER)
-				.append("Boot Bahn 4").append(DELIMITER).append("Status").append(StringUtils.LF);
-
-		for (Heat heat : this.heatsList) {
-			builder.append(heat.getNumber()).append(DELIMITER);
-			builder.append(heat.getRaceNumber()).append(DELIMITER);
-			builder.append(heat.getDevisionNumber()).append(DELIMITER);
-			builder.append("0").append(DELIMITER); // delay lane 1
-			builder.append("0").append(DELIMITER); // delay lane 2
-			builder.append("0").append(DELIMITER); // delay lane 3
-			builder.append("0").append(DELIMITER); // delay lane 4
-
-			List<HeatRegistration> heatRegs = heat.getEntriesSortedByLane();
-			for (HeatRegistration heatReg : heatRegs) {
-				builder.append(heatReg.getRegistration().getBib()).append(DELIMITER);
+		this.dbTask.run(progress -> {
+			return createCsv(this.heatsList);
+		}, dbResult -> {
+			try {
+				File file = FxUtils.showSaveDialog(this.refreshBtn.getScene().getWindow(),
+						getText("heats.csv.description"), "*.csv");
+				if (file != null) {
+					saveTextToFile(dbResult.getResult(), file);
+				}
+			} catch (Exception e) {
+				logger.log(Level.SEVERE, e.getMessage(), e);
+				FxUtils.showErrorMessage(e);
+			} finally {
+				disableButtons(false);
 			}
-
-			short laneCount = heat.getRace().getRaceMode().getLaneCount();
-			short diff = (short) (laneCount - heatRegs.size());
-			for (int i = 0; i < diff; i++) {
-				builder.append("0").append(DELIMITER);
-			}
-			builder.append("-").append(StringUtils.LF);
-		}
-
-		return builder.toString();
+		});
 	}
 
 	private void disableButtons(boolean disabled) {
@@ -134,8 +112,68 @@ public class HeatsController extends AbstractRegattaDAOController {
 
 	// static helpers
 
+	private static String createCsv(List<Heat> heats) {
+		StringBuilder builder = new StringBuilder(4096);
+		addCsvHeader(builder);
+
+		for (Heat heat : heats) {
+			builder.append(heat.getNumber()).append(DELIMITER);
+			builder.append(heat.getRaceNumber()).append(DELIMITER);
+			builder.append(heat.getDevisionNumber()).append(DELIMITER);
+
+			List<HeatRegistration> heatRegs = heat.getEntriesSortedByLane();
+			short laneCount = heat.getRace().getRaceMode().getLaneCount();
+			short diff = (short) (laneCount - heatRegs.size());
+
+			// add delays
+			if (heat.getRace().getAgeClass().isMasters()) {
+				for (HeatRegistration heatReg : heatRegs) {
+					String comment = heatReg.getRegistration().getComment();
+					if (StringUtils.isNotBlank(comment)) {
+						String delay = "0";
+						Matcher matcher = delayPattern.matcher(comment);
+						if (matcher.find()) {
+							delay = matcher.group();
+						}
+						builder.append(delay).append(DELIMITER);
+					} else {
+						builder.append("0").append(DELIMITER);
+					}
+				}
+				for (int i = 0; i < diff; i++) {
+					builder.append("0").append(DELIMITER);
+				}
+			} else {
+				for (int i = 0; i < laneCount; i++) {
+					builder.append("0").append(DELIMITER);
+				}
+			}
+
+			// add bibs
+			for (HeatRegistration heatReg : heatRegs) {
+				builder.append(heatReg.getRegistration().getBib()).append(DELIMITER);
+			}
+			for (int i = 0; i < diff; i++) {
+				builder.append("0").append(DELIMITER);
+			}
+
+			// add status
+			builder.append("-").append(StringUtils.LF);
+		}
+
+		return builder.toString();
+	}
+
+	private static void addCsvHeader(StringBuilder builder) {
+		builder.append("Index").append(DELIMITER).append("RennNr").append(DELIMITER).append("Abtlg").append(DELIMITER)
+				.append("Delay Bahn 1").append(DELIMITER).append("Delay Bahn 2").append(DELIMITER)
+				.append("Delay Bahn 3").append(DELIMITER).append("Delay Bahn 4").append(DELIMITER).append("Boot Bahn 1")
+				.append(DELIMITER).append("Boot Bahn 2").append(DELIMITER).append("Boot Bahn 3").append(DELIMITER)
+				.append("Boot Bahn 4").append(DELIMITER).append("Status").append(StringUtils.LF);
+	}
+
 	private static void saveTextToFile(String csvContent, File file) {
-		try (PrintWriter writer = new PrintWriter(file)) {
+		try (PrintWriter writer = new PrintWriter(file, StandardCharsets.UTF_8)) {
 			writer.println(csvContent);
 		} catch (IOException ex) {
 			logger.log(Level.SEVERE, null, ex);
