1ed85229ba084f070b84c5a755f1abc1c03d6d45
Markus Ofterdinger <markus.ofterdinger@sap.com>
2022-03-21 19:27:33 +0100

    cleanup

--- commons/src/main/java/module-info.java
+++ commons/src/main/java/module-info.java
@@ -1,9 +1,12 @@
 open module de.regatta_hd.commons {
 
 	requires com.google.guice;
+
+	// Java modules
 	requires java.desktop;
 	requires java.logging;
 
+	// exports
 	exports de.regatta_hd.commons;
 	exports de.regatta_hd.commons.concurrent;
 	exports de.regatta_hd.commons.impl to de.regatta_hd.aquarius;
