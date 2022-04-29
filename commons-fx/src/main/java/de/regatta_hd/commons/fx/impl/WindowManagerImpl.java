4d48a1374ac9612f3129f2d6106a89522dc04098
Markus Ofterdinger <markus.ofterdinger@sap.com>
2022-04-30 01:23:10 +0200

    window manager improvements

--- commons-fx/src/main/java/de/regatta_hd/commons/fx/impl/WindowManagerImpl.java
+++ commons-fx/src/main/java/de/regatta_hd/commons/fx/impl/WindowManagerImpl.java
@@ -5,6 +5,7 @@ import java.io.InputStream;
 import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 import java.util.ResourceBundle;
 
 import com.google.inject.Inject;
@@ -14,78 +15,79 @@ import de.regatta_hd.commons.fx.guice.FXMLLoaderFactory;
 import de.regatta_hd.commons.fx.stage.Controller;
 import de.regatta_hd.commons.fx.stage.WindowManager;
 import de.regatta_hd.commons.fx.util.FxUtils;
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
+import javafx.util.Pair;
 
 @Singleton
 public class WindowManagerImpl implements WindowManager {
 
+	private final FXMLLoaderFactory fxmlLoaderFactory;
+
+	private final Map<URL, Pair<Stage, Boolean>> stages = new HashMap<>(); // NOSONAR
+
 	@Inject
-	private FXMLLoaderFactory fxmlLoaderFactory;
-
-	private final Map<URL, Stage> stages = new HashMap<>(); // NOSONAR
+	WindowManagerImpl(FXMLLoaderFactory fxmlLoaderFactory) {
+		this.fxmlLoaderFactory = Objects.requireNonNull(fxmlLoaderFactory, "fxmlLoaderFactory must not be null");
+	}
 
 	@Override
-	public Stage newStage(URL resourceUrl, String title, ResourceBundle resources) {
-		Stage stage = this.stages.computeIfAbsent(resourceUrl, key -> {
+	public void loadPrimaryStage(Stage primaryStage, URL fxmlResourceUrl, String title, ResourceBundle bundle) {
+		this.stages.computeIfAbsent(fxmlResourceUrl, key -> {
 			try {
-				return newStageImpl(resourceUrl, title, resources);
+				loadStageImpl(primaryStage, fxmlResourceUrl, title, bundle);
+				return new Pair<>(primaryStage, Boolean.TRUE);
 			} catch (IOException e) {
 				throw new IllegalArgumentException(e);
 			}
 		});
-		stage.requestFocus();
-		return stage;
+		primaryStage.requestFocus();
 	}
 
 	@Override
-	public void loadStage(Stage stage, URL resourceUrl, String title, ResourceBundle resources) {
-		this.stages.computeIfAbsent(resourceUrl, key -> {
+	public Stage newStage(URL fxmlResourceUrl, String title, ResourceBundle bundle) {
+		Pair<Stage, Boolean> stage = this.stages.computeIfAbsent(fxmlResourceUrl, key -> {
 			try {
-				loadStageImpl(stage, resourceUrl, title, resources);
-				return stage;
+				return new Pair<>(newStageImpl(fxmlResourceUrl, title, bundle), Boolean.FALSE);
 			} catch (IOException e) {
 				throw new IllegalArgumentException(e);
 			}
 		});
-		stage.requestFocus();
+		stage.getKey().requestFocus();
+		return stage.getKey();
 	}
 
-	private Stage newStageImpl(URL resourceUrl, String title, ResourceBundle resources) throws IOException {
-		FXMLLoader loader = createLoader(resourceUrl, resources);
+	private Stage newStageImpl(URL fxmlResourceUrl, String title, ResourceBundle bundle) throws IOException {
+		FXMLLoader loader = createFXMLLoader(fxmlResourceUrl, bundle);
 
-		Stage stage = createStage(title, loader);
-
-		FxUtils.loadSizeAndPos(resourceUrl.getPath(), stage);
-		stage.show();
-
-		return stage;
-	}
-
-	private void loadStageImpl(Stage stage, URL resourceUrl, String title, ResourceBundle resources)
-			throws IOException {
-		FXMLLoader loader = createLoader(resourceUrl, resources);
-
-		initializeStage(stage, title, loader);
-
-		FxUtils.loadSizeAndPos(resourceUrl.getPath(), stage);
-		stage.show();
-	}
-
-	private FXMLLoader createLoader(URL resourceUrl, ResourceBundle resources) {
-		FXMLLoader loader = this.fxmlLoaderFactory.newFXMLLoader();
-		loader.setLocation(resourceUrl);
-		loader.setResources(resources);
-		return loader;
-	}
-
-	private Stage createStage(String title, FXMLLoader loader) throws IOException {
 		Stage stage = new Stage();
 		initializeStage(stage, title, loader);
+
+		FxUtils.loadSizeAndPos(fxmlResourceUrl.getPath(), stage);
+		stage.show();
+
 		return stage;
+	}
+
+	private void loadStageImpl(Stage stage, URL fxmlResourceUrl, String title, ResourceBundle bundle)
+			throws IOException {
+		FXMLLoader loader = createFXMLLoader(fxmlResourceUrl, bundle);
+
+		initializeStage(stage, title, loader);
+
+		FxUtils.loadSizeAndPos(fxmlResourceUrl.getPath(), stage);
+		stage.show();
+	}
+
+	private FXMLLoader createFXMLLoader(URL fxmlResourceUrl, ResourceBundle bundle) {
+		FXMLLoader loader = this.fxmlLoaderFactory.newFXMLLoader();
+		loader.setLocation(fxmlResourceUrl);
+		loader.setResources(bundle);
+		return loader;
 	}
 
 	private void initializeStage(Stage stage, String title, FXMLLoader loader) throws IOException {
@@ -97,13 +99,19 @@ public class WindowManagerImpl implements WindowManager {
 
 		// When the stage closes store the current size and window location.
 		stage.addEventHandler(WindowEvent.WINDOW_CLOSE_REQUEST, event -> {
-			URL location = loader.getLocation();
-			FxUtils.storeSizeAndPos(location.getPath(), stage);
-			this.stages.remove(location);
-
 			Controller controller = loader.getController();
 			if (controller != null) {
 				controller.shutdown();
+			}
+
+			URL location = loader.getLocation();
+			FxUtils.storeSizeAndPos(location.getPath(), stage);
+
+			Pair<Stage, Boolean> closedStage = this.stages.remove(location);
+
+			// if primary window is closed, exit application
+			if (closedStage.getValue().booleanValue()) {
+				Platform.runLater(Platform::exit);
 			}
 		});
 	}
