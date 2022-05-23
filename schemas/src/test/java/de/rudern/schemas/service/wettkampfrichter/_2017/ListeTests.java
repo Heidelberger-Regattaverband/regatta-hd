b4a6e21a75c7de4e831fe8a67a53e5bbcde16058
Markus Ofterdinger <markus.ofterdinger@sap.com>
2022-05-23 20:40:32 +0200

    fixed xml

--- schemas/src/test/java/de/rudern/schemas/service/wettkampfrichter/_2017/ListeTests.java
+++ schemas/src/test/java/de/rudern/schemas/service/wettkampfrichter/_2017/ListeTests.java
@@ -4,21 +4,40 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
 
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParserFactory;
+import javax.xml.transform.Source;
+import javax.xml.transform.sax.SAXSource;
+
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xml.sax.XMLReader;
 
-import jakarta.xml.bind.JAXB;
+import jakarta.xml.bind.JAXBContext;
+import jakarta.xml.bind.JAXBException;
+import jakarta.xml.bind.Unmarshaller;
 
 class ListeTests {
 
 	private static Liste liste;
 
 	@BeforeAll
-	static void setUpBeforeClass() throws IOException {
+	static void setUpBeforeClass() throws IOException, JAXBException, SAXException, ParserConfigurationException {
 		try (InputStream input = ListeTests.class.getResourceAsStream("/wkr-2022-05-18.xml")) {
-			liste = JAXB.unmarshal(input, Liste.class);
+			JAXBContext jaxbContext = JAXBContext.newInstance(Liste.class);
+			Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
+
+			SAXParserFactory saxFactory = SAXParserFactory.newInstance();
+			saxFactory.setNamespaceAware(true);
+			saxFactory.setValidating(false);
+			XMLReader xmlReader = saxFactory.newSAXParser().getXMLReader();
+			Source source = new SAXSource(xmlReader, new InputSource(input));
+
+			liste = (Liste) unmarshaller.unmarshal(source);
 		}
 	}
 
@@ -29,7 +48,7 @@ class ListeTests {
 
 	@Test
 	void testGetWettkampfrichter() {
-		List<Object> wettkampfrichter = liste.getAny();
+		List<TWKR> wettkampfrichter = liste.getWettkampfrichter();
 
 		Assertions.assertFalse(wettkampfrichter.isEmpty());
 	}
