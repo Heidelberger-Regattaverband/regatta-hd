4ff48aa84e9361d970e372d18b0e93d1c97417f2
Markus Ofterdinger <markus.ofterdinger@sap.com>
2022-06-08 15:38:54 +0200

    moved to releng/webstart (#138)
    * moved to releng/webstart
    
    * appassembler

---
+++ releng/app/src/main/java/de/regatta_hd/app/Main.java
@@ -0,0 +1,11 @@
+package de.regatta_hd.app;
+
+import de.regatta_hd.ui.RegattaHD;
+
+public class Main {
+
+	public static void main(String[] args) {
+		RegattaHD.main(args);
+	}
+
+}
---
+++ releng/app/src/main/java/module-info.java
@@ -0,0 +1,5 @@
+module de.regatta_hd.app {
+
+	// regatta_hd modules
+	requires de.regatta_hd.ui;
+}
