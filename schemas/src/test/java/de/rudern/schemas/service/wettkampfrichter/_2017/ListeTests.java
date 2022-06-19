bfa344c9c68485abbcaec8737c71c7cee5cb6c12
Markus Ofterdinger <markus.ofterdinger@sap.com>
2022-06-19 10:20:03 +0200

    fixed eclipse warnings

--- schemas/src/test/java/de/rudern/schemas/service/wettkampfrichter/_2017/ListeTests.java
+++ schemas/src/test/java/de/rudern/schemas/service/wettkampfrichter/_2017/ListeTests.java
@@ -4,13 +4,10 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
 
-import javax.xml.parsers.ParserConfigurationException;
-
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.xml.sax.SAXException;
 
 import de.regatta_hd.schemas.xml.XMLDataLoader;
 import jakarta.xml.bind.JAXBException;
@@ -20,7 +17,7 @@ class ListeTests {
 	private static Liste liste;
 
 	@BeforeAll
-	static void setUpBeforeClass() throws IOException, JAXBException, SAXException, ParserConfigurationException {
+	static void setUpBeforeClass() throws IOException, JAXBException {
 		try (InputStream input = ListeTests.class.getResourceAsStream("/wkr-2022-05-18.xml")) {
 			liste = XMLDataLoader.loadWkrListe(input);
 		}
