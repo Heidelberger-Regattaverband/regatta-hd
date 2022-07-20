059c5cd2005a6beebbfacb56b6fe2ba8c7b5894d
Markus Ofterdinger <markus.ofterdinger@sap.com>
2022-07-20 11:31:35 +0200

    start signal and traffic light config

--- ui/src/main/java/de/regatta_hd/ui/UIModule.java
+++ ui/src/main/java/de/regatta_hd/ui/UIModule.java
@@ -23,11 +23,15 @@ public class UIModule extends AbstractModule {
 
 	public static final String CONFIG_SHOW_ID_COLUMN = "config.showIdColumn";
 
-	public static final String CONFIG_SERIAL_PORT_NAME = "config.serialPortName";
+	public static final String CONFIG_SERIAL_PORT_START_SIGNAL = "config.serialPort.startSignal";
 
-	private SimpleBooleanProperty showIdColumnProperty;
+	public static final String CONFIG_SERIAL_PORT_TRAFFIC_LIGHT = "config.serialPort.trafficLight";
 
-	private SimpleStringProperty serialPortNameProperty;
+	private SimpleBooleanProperty showIdColumn;
+
+	private SimpleStringProperty serialPortStartSignal;
+
+	private SimpleStringProperty serialPortTrafficLight;
 
 	@Override
 	protected void configure() {
@@ -37,10 +41,9 @@ public class UIModule extends AbstractModule {
 	@Provides
 	@Named(CONFIG_SHOW_ID_COLUMN)
 	BooleanProperty getShowIdColumn(ConfigService configService) {
-		if (this.showIdColumnProperty == null) {
-			this.showIdColumnProperty = new SimpleBooleanProperty(
-					configService.getBooleanProperty(CONFIG_SHOW_ID_COLUMN));
-			this.showIdColumnProperty.addListener((obs, oldValue, newValue) -> {
+		if (this.showIdColumn == null) {
+			this.showIdColumn = new SimpleBooleanProperty(configService.getBooleanProperty(CONFIG_SHOW_ID_COLUMN));
+			this.showIdColumn.addListener((obs, oldValue, newValue) -> {
 				try {
 					configService.setProperty(CONFIG_SHOW_ID_COLUMN, newValue.booleanValue());
 				} catch (IOException e) {
@@ -48,23 +51,41 @@ public class UIModule extends AbstractModule {
 				}
 			});
 		}
-		return this.showIdColumnProperty;
+		return this.showIdColumn;
 	}
 
 	@Provides
-	@Named(CONFIG_SERIAL_PORT_NAME)
-	StringProperty getSerialPortName(ConfigService configService) {
-		if (this.serialPortNameProperty == null) {
-			this.serialPortNameProperty = new SimpleStringProperty(configService.getProperty(CONFIG_SERIAL_PORT_NAME));
-			this.serialPortNameProperty.addListener((obs, oldValue, newValue) -> {
+	@Named(CONFIG_SERIAL_PORT_START_SIGNAL)
+	StringProperty getSerialPortStartSignal(ConfigService configService) {
+		if (this.serialPortStartSignal == null) {
+			this.serialPortStartSignal = new SimpleStringProperty(
+					configService.getProperty(CONFIG_SERIAL_PORT_START_SIGNAL));
+			this.serialPortStartSignal.addListener((obs, oldValue, newValue) -> {
 				try {
-					configService.setProperty(CONFIG_SERIAL_PORT_NAME, newValue);
+					configService.setProperty(CONFIG_SERIAL_PORT_START_SIGNAL, newValue);
 				} catch (IOException e) {
 					logger.log(Level.SEVERE, e.getMessage(), e);
 				}
 			});
 		}
-		return this.serialPortNameProperty;
+		return this.serialPortStartSignal;
+	}
+
+	@Provides
+	@Named(CONFIG_SERIAL_PORT_TRAFFIC_LIGHT)
+	StringProperty getSerialPortTrafficLight(ConfigService configService) {
+		if (this.serialPortTrafficLight == null) {
+			this.serialPortTrafficLight = new SimpleStringProperty(
+					configService.getProperty(CONFIG_SERIAL_PORT_TRAFFIC_LIGHT));
+			this.serialPortTrafficLight.addListener((obs, oldValue, newValue) -> {
+				try {
+					configService.setProperty(CONFIG_SERIAL_PORT_TRAFFIC_LIGHT, newValue);
+				} catch (IOException e) {
+					logger.log(Level.SEVERE, e.getMessage(), e);
+				}
+			});
+		}
+		return this.serialPortTrafficLight;
 	}
 
 }
--- ui/src/main/java/de/regatta_hd/ui/pane/ConfigController.java
+++ ui/src/main/java/de/regatta_hd/ui/pane/ConfigController.java
@@ -21,28 +21,69 @@ public class ConfigController extends AbstractBaseController {
 	@FXML
 	private CheckBox showIdColumnCbox;
 	@FXML
-	private ComboBox<SerialPort> serialPortsCBox;
+	private ComboBox<SerialPort> serialPortStartSignalCBox;
+	@FXML
+	private ComboBox<SerialPort> serialPortTrafficLightCBox;
 
 	@Inject
 	@Named(UIModule.CONFIG_SHOW_ID_COLUMN)
 	private BooleanProperty showIdColumn;
 	@Inject
-	@Named(UIModule.CONFIG_SERIAL_PORT_NAME)
-	private StringProperty serialPortName;
+	@Named(UIModule.CONFIG_SERIAL_PORT_START_SIGNAL)
+	private StringProperty serialPortStartSignal;
+	@Inject
+	@Named(UIModule.CONFIG_SERIAL_PORT_TRAFFIC_LIGHT)
+	private StringProperty serialPortTrafficLight;
 
-	private final ObservableList<SerialPort> serialPortsList = FXCollections.observableArrayList();
+	private final ObservableList<SerialPort> serialPortsStartSignalList = FXCollections.observableArrayList();
+	private final ObservableList<SerialPort> serialPortsTrafficLightList = FXCollections.observableArrayList();
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		this.showIdColumnCbox.selectedProperty().bindBidirectional(this.showIdColumn);
 
-		this.serialPortsList.addAll(SerialPort.getCommPorts());
-		this.serialPortsCBox.setItems(this.serialPortsList);
+		this.serialPortsStartSignalList.addAll(SerialPort.getCommPorts());
+		this.serialPortStartSignalCBox.setItems(this.serialPortsStartSignalList);
+		int index = -1;
+		for (int i = 0; i < this.serialPortsStartSignalList.size(); i++) {
+			if (this.serialPortsStartSignalList.get(i).getSystemPortPath().equals(this.serialPortStartSignal.get())) {
+				index = i;
+				break;
+			}
+		}
+		this.serialPortStartSignalCBox.getSelectionModel().select(index);
+
+		this.serialPortsTrafficLightList.addAll(SerialPort.getCommPorts());
+		this.serialPortTrafficLightCBox.setItems(this.serialPortsStartSignalList);
+		index = -1;
+		for (int i = 0; i < this.serialPortsTrafficLightList.size(); i++) {
+			if (this.serialPortsTrafficLightList.get(i).getSystemPortPath().equals(this.serialPortTrafficLight.get())) {
+				index = i;
+				break;
+			}
+		}
+		this.serialPortTrafficLightCBox.getSelectionModel().select(index);
 	}
 
 	@Override
 	public void shutdown() {
 		// nothing to clean-up yet
+	}
+
+	@FXML
+	void handleSerialPortStartSignalOnAction() {
+		SerialPort serialPort = this.serialPortStartSignalCBox.getSelectionModel().getSelectedItem();
+		if (serialPort != null) {
+			this.serialPortStartSignal.set(serialPort.getSystemPortPath());
+		}
+	}
+
+	@FXML
+	void handleSerialPortTrafficLightOnAction() {
+		SerialPort serialPort = this.serialPortTrafficLightCBox.getSelectionModel().getSelectedItem();
+		if (serialPort != null) {
+			this.serialPortTrafficLight.set(serialPort.getSystemPortPath());
+		}
 	}
 
 }
