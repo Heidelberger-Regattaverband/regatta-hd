db89d03c9e915a0c58da09825525def2f9a693a3
Markus Ofterdinger <markus.ofterdinger@sap.com>
2022-07-23 17:28:09 +0200

    skip generation of getter/setter for finalResult

--- aquarius/src/main/java/de/regatta_hd/aquarius/model/HeatRegistration.java
+++ aquarius/src/main/java/de/regatta_hd/aquarius/model/HeatRegistration.java
@@ -14,6 +14,7 @@ import jakarta.persistence.OneToMany;
 import jakarta.persistence.OrderBy;
 import jakarta.persistence.Table;
 import jakarta.persistence.Transient;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -57,6 +58,8 @@ public class HeatRegistration {
 	private Set<Result> results;
 
 	@Transient
+	@Getter(value = AccessLevel.NONE)
+	@Setter(value = AccessLevel.NONE)
 	private Result finalResult;
 
 	/**
